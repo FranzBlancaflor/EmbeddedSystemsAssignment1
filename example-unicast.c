1bb383502b460129bd23e9b1148c6975c3fc93aa
FranzBlancaflor <43249642+FranzBlancaflor@users.noreply.github.com>
2018-11-13 19:40:46 +0000

    Update example-unicast.c

--- example-unicast.c
+++ example-unicast.c
@@ -39,12 +39,31 @@ static const struct unicast_callbacks unicast_callbacks = {recv_uc, sent_uc};
 static struct unicast_conn uc;
 /*---------------------------------------------------------------------------*/
 
+static int tempVal;
+
+static int tempVals[4];
 static void
 broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
 {
-  printf("broadcast message received from %d.%d: '%s'\n",
-         from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
+    printf("broadcast message received from %d.%d: '%s'\n",
+           from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
+
+    //Sending the temperature to the sink
+
+    linkaddr_t destination;
+    char str[10];
+    sprintf(str, "%d", tempVal);
+    packetbuf_copyfrom(str, 5);
+    destination.u8[0] = from->u8[0];
+    destination.u8[1] = from->u8[1];
+    if(!linkaddr_cmp(&destination, &linkaddr_node_addr)) {
+        unicast_send(&uc, &destination);
+    }
+
 }
+static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
+static struct broadcast_conn broadcast;
+
 
 PROCESS_THREAD(example_unicast_process, ev, data)
 {
@@ -58,6 +77,7 @@ PROCESS_THREAD(example_unicast_process, ev, data)
 
   while(1) {
     static struct etimer et;
+
     linkaddr_t addr;
 
     SENSORS_ACTIVATE(sht11_sensor);
@@ -66,13 +86,15 @@ PROCESS_THREAD(example_unicast_process, ev, data)
     
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
-    packetbuf_copyfrom("Hello", 5);
-    addr.u8[0] = 1;
-    addr.u8[1] = 0;
-    if(!linkaddr_cmp(&addr, &linkaddr_node_addr)) {
-      unicast_send(&uc, &addr);
-    }
+    int val = sht11_sensor.value(SHT11_SENSOR_TEMP);
+    
+    tempVal = (-39.60 + 0.01*val);
 
+    }
+    
+    etimer_reset(&et);
+
+    SENSORS_DEACTIVATE(sht11_sensor);
   }
 
   PROCESS_END();
