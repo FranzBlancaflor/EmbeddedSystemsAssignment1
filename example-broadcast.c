6d9dcb092a475c92ab7b10798009d97f52ffd4ce
FranzBlancaflor <43249642+FranzBlancaflor@users.noreply.github.com>
2018-11-14 18:33:20 +0000

    Update example-broadcast.c

--- example-broadcast.c
+++ example-broadcast.c
@@ -18,32 +18,41 @@
 PROCESS(example_broadcast_process, "Broadcast example");
 AUTOSTART_PROCESSES(&example_broadcast_process);
 /*---------------------------------------------------------------------------*/
+
+//When the collector receives a broadcast message
 static void
 broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
 {
  
 }
+
+//The struct that tells the collector what methods to invoke
 static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
 static struct broadcast_conn broadcast;
 
+//When the collector receives a unicast message
 static void
 recv_uc(struct unicast_conn *c, const linkaddr_t *from) 
 {
   printf("Unicast message received from %d.%d: '%s'\n",
          from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
 }
+
+//The struct that tells the collector what methods to invoke
 static const struct unicast_callbacks unicast_callbacks = { recv_uc };
 static struct unicast_conn uc;
 
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(example_broadcast_process, ev, data)
 {
+  //Making an etimer
   static struct etimer et;
 
   PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
 
   PROCESS_BEGIN();
 
+  //Open broadcast connection in band 129 and unicast connection in band 135
   broadcast_open(&broadcast, 129, &broadcast_call);
 
   unicast_open(&uc, 135, &unicast_callbacks);	
@@ -55,6 +64,9 @@ PROCESS_THREAD(example_broadcast_process, ev, data)
 
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
+    /*
+	Every 2-4 seconds sends out a packet as a broadcast
+    */
     packetbuf_copyfrom("Yo", 3);
     broadcast_send(&broadcast);
     printf("broadcast message sent\n");
